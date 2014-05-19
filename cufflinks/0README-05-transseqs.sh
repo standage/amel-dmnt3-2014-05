54755dd86de424c801be039be738295fcc32a566
Daniel Standage <daniel.standage@gmail.com>
2014-05-19 15:52:49 -0400

    More cufflinks docs

---
+++ cufflinks/0README-05-transseqs.sh
@@ -0,0 +1,4 @@
+TDC=/usr/local/src/NGS-DIR/TRANSDECODER/transdecoder_rel16JAN2014
+
+$TDC/util/cufflinks_gtf_genome_to_cdna_fasta.pl assemblies/merged/merged.gtf iloci/Amel.iloci.fa > assemblies/merged/merged.fasta
+
